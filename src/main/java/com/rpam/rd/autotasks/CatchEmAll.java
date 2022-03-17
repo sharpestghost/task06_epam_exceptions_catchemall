261b49484c1c6a261378b743e6398db0de0158f1
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-18 00:12:47 +0300

    fixes

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -11,31 +11,25 @@ public final class CatchEmAll {
     private static final String NFE_EXCEPTION_STATUS = "NumberFormatException";
     private static final String RESOURCE_MISSING_MESSAGE = "Resource is missing";
     private static final String RESOURCE_ERROR_MESSAGE = "Resource error";
-    public static Exception exception;
+    public static Exception exception = null;
 
     private CatchEmAll() {
     }
 
-    public static void riskyMethod()  {
+    public static void riskyMethod() throws Exception {
         if (exception != null) {
-            try {
-                throw exception;
-            } catch (FileNotFoundException e) {
-                throw new IllegalArgumentException(RESOURCE_MISSING_MESSAGE, e);
-            } catch (IOException e) {
-                throw new IllegalArgumentException(RESOURCE_ERROR_MESSAGE, e);
-            } catch (ArithmeticException | NumberFormatException e) {
-                System.err.print(e.getMessage());
-            } catch (Exception e) {
-                doNothing();
-            }
+            throw exception;
         }
     }
 
-    private static void doNothing() {
-    }
+
 
     public static void main(String[] args) {
+        setException(args);
+        printMessage();
+    }
+
+    public static void setException(String[] args) {
         if (args.length > 0) {
             switch (args[0]) {
                 case IO_EXCEPTION_STATUS:
@@ -53,8 +47,25 @@ public final class CatchEmAll {
                 default:
                     break;
             }
-            riskyMethod();
         }
+    }
+
+    public static void printMessage() {
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(RESOURCE_MISSING_MESSAGE, e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(RESOURCE_ERROR_MESSAGE, e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.print(e.getMessage());
+        } catch (Exception e) {
+            doNothing();
+        }
+    }
+
+    private static void doNothing() {
+        //response to any other exceptions
     }
 
 }
