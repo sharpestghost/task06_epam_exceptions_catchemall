3a6b091e6e089dc23d94fc62fc7be259bacc1dbb
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-17 22:51:30 +0300

    raw

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -1,17 +1,62 @@
 package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
-public class CatchEmAll {
+public final class CatchEmAll {
 
-    //You may set another exception in this field;
-    static Exception exception = new FileNotFoundException();
+    private static final String IO_EXCEPTION_STATUS = "IOException";
+    private static final String FNF_EXCEPTION_STATUS = "FileNotFoundException";
+    private static final String ARITHMETIC_EXCEPTION_STATUS = "ArithmeticException";
+    private static final String NFE_EXCEPTION_STATUS = "NumberFormatException";
+    private static final String RESOURCE_MISSING_MESSAGE = "Resource is missing";
+    private static final String RESOURCE_ERROR_MESSAGE = "Resource error";
+    public static Exception exception;
+
+    private CatchEmAll() {
+        exception = null;
+    }
 
     public static void riskyMethod() throws Exception {
-        throw exception;
+        if (exception != null) {
+            throw exception;
+        }
     }
 
-    public static void main(String[] args) throws Exception {
-        riskyMethod();
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            switch (args[0]) {
+                case IO_EXCEPTION_STATUS:
+                    exception = new IOException(IO_EXCEPTION_STATUS);
+                    break;
+                case FNF_EXCEPTION_STATUS:
+                    exception = new FileNotFoundException(FNF_EXCEPTION_STATUS);
+                    break;
+                case ARITHMETIC_EXCEPTION_STATUS:
+                    exception = new ArithmeticException(ARITHMETIC_EXCEPTION_STATUS);
+                    break;
+                case NFE_EXCEPTION_STATUS:
+                    exception = new NumberFormatException(ARITHMETIC_EXCEPTION_STATUS);
+                    break;
+                default:
+                    break;
+            }
+        }
+        catchException();
+    }
+
+    private static void catchException() {
+        try {
+            riskyMethod();
+        } catch (FileNotFoundException e) {
+            throw new IllegalArgumentException(RESOURCE_MISSING_MESSAGE, e);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(RESOURCE_ERROR_MESSAGE, e);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.err.print(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
