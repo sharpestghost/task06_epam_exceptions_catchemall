cc7e596884c7f2f1abdac5663586237349501bef
borrowsorrow <51077910+borrowsorrow@users.noreply.github.com>
2022-03-18 09:33:44 +0300

    remove unused imports

--- src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
+++ src/main/java/com/rpam/rd/autotasks/CatchEmAll.java
@@ -2,7 +2,6 @@ package com.rpam.rd.autotasks;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 
 public final class CatchEmAll {
 
